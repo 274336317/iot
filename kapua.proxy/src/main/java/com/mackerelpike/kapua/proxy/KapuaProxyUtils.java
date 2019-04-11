a60be4fb60a0f60615e5c0448df0d58588be3a5a
mackerel <274336317@qq.com>
2019-04-11 10:14:08 +0800

    添加了KapuaProxyUtils

--- kapua.proxy/src/main/java/com/mackerelpike/kapua/proxy/KapuaProxyUtils.java
+++ kapua.proxy/src/main/java/com/mackerelpike/kapua/proxy/KapuaProxyUtils.java
@@ -1,12 +1,26 @@
 package com.mackerelpike.kapua.proxy;
 
+import com.google.gson.Gson;
+
 public class KapuaProxyUtils 
 {
 
+	public static String LOGIN_URL = "http://192.168.0.111:8081/v1/authentication/user";
+	
 	public static UserInfo Login(KapuaProxy proxy)
 	{
+		UserInfo user = null;
 		
-		return null;
+		String param="{ \"username\": \""+proxy.getUserName()+"\", \"password\": \""+proxy.getPassword()+"\" }";
+		
+		String result = HttpHelper.httpPost(LOGIN_URL, param);
+		if(result != null)
+		{
+			Gson g = new Gson();
+			user = g.fromJson(result, UserInfo.class);
+		}
+		
+		return user;
 	}
 	
 }
